1650c598ef7aff45dcaa9b8276a396ddabfe1d88
Aaron Jacobs <aaron@carnotinteractive.com>
2013-03-13 22:40:21 -0700

    Started work on health bar

--- src/edu/calpoly/csc/pulseman/MainActivity.java
+++ src/edu/calpoly/csc/pulseman/MainActivity.java
@@ -1,12 +1,18 @@
 package edu.calpoly.csc.pulseman;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import edu.calpoly.csc.pulseman.ConnectionHandler.ConnectionStatusListener;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
@@ -18,17 +24,22 @@ public class MainActivity extends Activity
 	public static final int SOCKET_TIMEOUT = 10000, PORT = 42000;
 	public static final String IP_ADDRESS = "ipAddress";
 
+	private SurfaceView barSurfaceView;
 	private RelativeLayout layout;
 	private RelativeLayout statusLayout;
 	private ImageView heartImageView;
 	private TextView statusText;
 	private String ipAddress;
+	
+	private Timer renderTimer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		barSurfaceView = (SurfaceView)findViewById(R.id.barSurfaceView);
 
 		Bundle extras = getIntent().getExtras();
 		ipAddress = extras.getString(IP_ADDRESS);
@@ -93,6 +104,9 @@ public class MainActivity extends Activity
 	public void onResume()
 	{
 		super.onResume();
+		
+		renderTimer = new Timer("Renderer");
+		renderTimer.schedule(new Renderer(barSurfaceView.getHolder()), 0, 15);
 
 		new Thread(new Runnable()
 		{
@@ -133,6 +147,8 @@ public class MainActivity extends Activity
 	public void onPause()
 	{
 		super.onPause();
+		
+		renderTimer.cancel();
 
 		new Thread(new Runnable()
 		{
@@ -144,5 +160,40 @@ public class MainActivity extends Activity
 		}, "Disconnect thread").start();
 
 		finish();
+	}
+
+	private static class Renderer extends TimerTask
+	{
+		private SurfaceHolder holder;
+		private Paint paint;
+		float value = 0.0f;
+		private double f = 0.0;
+
+		public Renderer(SurfaceHolder holder)
+		{
+			this.holder = holder;
+			this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		}
+
+		@Override
+		public void run()
+		{
+			if(holder.getSurface().isValid())
+			{
+				Canvas canvas = holder.lockCanvas();
+				canvas.drawColor(Color.BLACK);
+				
+				value = ((float)Math.sin(f) + 1.0f) / 2.0f;
+
+				paint.setStyle(Paint.Style.FILL);
+				paint.setColor(Color.rgb(255 - (int)(value * 255), (int)(value * 255), 0));
+
+				canvas.drawRect(0.0f, 25.0f, value * canvas.getWidth(), canvas.getHeight() - 25.0f, paint);
+				
+				f += 0.05;
+
+				holder.unlockCanvasAndPost(canvas);
+			}
+		}
 	}
 }
